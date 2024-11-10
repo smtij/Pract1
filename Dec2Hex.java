bcc8fcf41ccde466ef884c66384f9b627360a96c
smtij <smithjason976@gmail.com>
2024-11-10 22:36:17 +0000

    Added Comments to Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,18 +1,33 @@
+/**
+ * The Dec2Hex class provides a method to convert a decimal number to its hexadecimal representation.
+ */
 public class Dec2Hex {
-    public static String convert(int num) {
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        int rem;
-        String hexadecimal = "";
 
+    /**
+     * Converts a given decimal number to its hexadecimal representation.
+     *
+     * @param num The decimal number to be converted.
+     * @return The hexadecimal representation of the given decimal number as a String.
+     */
+    public static String convert(int num) {
+        // Array of hexadecimal characters from '0' to 'F'
+        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem; // Stores the remainder after division by 16
+        String hexadecimal = ""; // Stores the resulting hexadecimal representation
+
+        // Loop until the number is reduced to zero
         while (num != 0) {
-            rem = num % 16;
-            hexadecimal = ch[rem] + hexadecimal;
-            num = num / 16;
+            rem = num % 16; // Get the remainder when dividing by 16
+            hexadecimal = ch[rem] + hexadecimal; // Append the corresponding hex character at the beginning
+            num = num / 16; // Divide the number by 16 to continue converting
         }
 
+        // If the input number was 0, set hexadecimal to "0"
         if (hexadecimal.equals("")) {
             hexadecimal = "0";
         }
+
+        // Return the final hexadecimal representation
         return hexadecimal;
     }
 }
