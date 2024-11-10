11d68e2dee4310474c24186d028f5cb6e375e80c
smtij <smithjason976@gmail.com>
2024-11-10 23:37:05 +0000

    Added Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,33 +1,26 @@
-/**
- * The Dec2Hex class provides a method to convert a decimal number to its hexadecimal representation.
- */
-public class Dec2Hex {
 
-    /**
-     * Converts a given decimal number to its hexadecimal representation.
-     *
-     * @param num The decimal number to be converted.
-     * @return The hexadecimal representation of the given decimal number as a String.
-     */
-    public static String convert(int num) {
-        // Array of hexadecimal characters from '0' to 'F'
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        int rem; // Stores the remainder after division by 16
-        StringBuilder hexadecimal = new StringBuilder(); // Stores the resulting hexadecimal representation
+import java.util.Scanner;
 
-        // Loop until the number is reduced to zero
-        while (num != 0) {
-            rem = num % 16; // Get the remainder when dividing by 16
-            hexadecimal.insert(0, ch[rem]); // Insert the corresponding hex character at the beginning
-            num = num / 16; // Divide the number by 16 to continue converting
+class Dec2Hex
+{
+public static int Arg1;
+    public static void main(String args[])    {
+        Arg1 = Integer.parseInt(args[0]);
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal="";
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+        while(num != 0)
+        {
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
         }
 
-        // If the input number was 0, set hexadecimal to "0"
-        if (hexadecimal.length() == 0) {
-            hexadecimal.append("0");
-        }
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
 
-        // Return the final hexadecimal representation
-        return hexadecimal.toString();
     }
 }
+
