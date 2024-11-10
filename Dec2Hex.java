7a5e47505ad834af276afcc84c5a2ce405981c2d
smtij <159401251+smtij@users.noreply.github.com>
2024-11-10 01:06:13 +0000

    Update Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -18,7 +18,7 @@ public class Dec2Hex {
     }
 
     // Method to convert decimal to hexadecimal udfiuedfuiehg
-//dfhnhffrgergreg
+//dfhnhffrgergregggergregre
     public static String convert(int num) {
         char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         int rem;
